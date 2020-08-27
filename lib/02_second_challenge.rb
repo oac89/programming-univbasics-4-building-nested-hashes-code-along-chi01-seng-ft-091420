1f59157792cbbbee6e5951628296fc976c1cace9
Oscar A Castro <oacastro2@gmail.com>
2020-08-27 19:29:02 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,19 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriacrh => {},
-      :hero => {},
-      :hero_friends => {}
-      }, 
-    :capulet => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friend => {}, 
-      }
-    
-  }
-
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
